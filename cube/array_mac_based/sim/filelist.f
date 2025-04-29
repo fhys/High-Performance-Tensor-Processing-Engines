a83d42da2a30cd8b302956207e1b18981ec1a44d
wqzustc <86584841+wqzustc@users.noreply.github.com>
2025-04-29 19:49:57 +0800

    Delete cube directory

--- cube/array_mac_based/sim/filelist.f
+++
@@ -1,5 +0,0 @@
-/home/vcs/workspace/wqz/code/experiment/HPCA2025/OPT1/cube/array_mac_based/sim/timescale.sv
-/home/vcs/workspace/wqz/code/experiment/HPCA2025/OPT1/cube/array_mac_based/sim/test_mac_tc_array.sv
-/home/vcs/workspace/wqz/code/experiment/HPCA2025/OPT1/cube/array_mac_based/get_pipline_mulwidth.v
-/home/vcs/workspace/wqz/code/experiment/HPCA2025/OPT1/cube/array_mac_based/pe.v
-/home/vcs/workspace/wqz/code/experiment/HPCA2025/OPT1/cube/array_mac_based/top.v
