8cad0e91d3d889a9245b5250e3a3a6b18e535922
wqzustc <86584841+wqzustc@users.noreply.github.com>
2025-04-29 19:50:15 +0800

    Delete systolic_array_ws directory

--- systolic_array_ws/array_mac_based/sim/filelist.f
+++
@@ -1,6 +0,0 @@
-
-/home/vcs/workspace/wqz/code/experiment/HPCA2025/OPT1/systolic_array_ws/array_mac_based/sim/timescale.sv
-/home/vcs/workspace/wqz/code/experiment/HPCA2025/OPT1/systolic_array_ws/array_mac_based/get_pipline_mulwidth.v
-/home/vcs/workspace/wqz/code/experiment/HPCA2025/OPT1/systolic_array_ws/array_mac_based/top.v
-/home/vcs/workspace/wqz/code/experiment/HPCA2025/OPT1/systolic_array_ws/array_mac_based/PE.v
-/home/vcs/workspace/wqz/code/experiment/HPCA2025/OPT1/systolic_array_ws/array_mac_based/sim/test_ws_array.sv
--- systolic_array_ws/array_mac_based/syn/filelist.f
+++
@@ -1,2 +0,0 @@
-/home/vcs/workspace/wqz/code/experiment/HPCA2025/OPT1/systolic_array_ws/array_mac_based/top.v
-/home/vcs/workspace/wqz/code/experiment/HPCA2025/OPT1/systolic_array_ws/array_mac_based/PE.v
