538f493d237a4fb64eb6f342f38f75f9218823af
wqzustc <86584841+wqzustc@users.noreply.github.com>
2025-05-19 00:32:33 +0800

    Update test_opt4c_column_array.sv

--- OPT3_OPT4C/array/sim/test_opt4c_column_array.sv
+++ OPT3_OPT4C/array/sim/test_opt4c_column_array.sv
@@ -302,7 +302,7 @@ endtask
 
 // benchmark for average partial product
 always @(posedge clk) begin
-  if (encode_valid) begin
+  if (encode_valid & !$isunknown(cal_cycle)) begin
     sum <= sum + cal_cycle; 
     cnt <= cnt + 1;         
   end
